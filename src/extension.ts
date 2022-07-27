3cdaf2c85719582a205f37eeab310352c6bae207
Lola2018 <eftuwakjira@yahoo.com>
2022-07-27 08:39:27 -0700

    First commit

--- src/extension.ts
+++ src/extension.ts
@@ -1,173 +1,50 @@
-/**
- *
- * UX Guidelines Documentation for VS Code
- *  - https://code.visualstudio.com/api/ux-guidelines/overview
-/**
- * ? ====== IMPORT SECTION ======
- *
- * With the vscode extension library (the library used to create extensions) you
- * need to import classes and functions to make it work. This is where we
- * import theses things.
- */
-import {
-  commands,
-  window,
-  workspace,
-  TreeDataProvider,
-  TreeItem,
-  ProviderResult,
-  TreeItemCollapsibleState,
-} from "vscode";
+import { window, workspace, Range, Position } from "vscode";
 
-/**
- * Library to parse the Typescript.
- * Link : https://ts-morph.com/
- */
-import {
-  LabeledStatement,
-  NoSubstitutionTemplateLiteral,
-  Project,
-} from "ts-morph";
+import type { ExtensionContext, TextEditor, DecorationOptions } from "vscode";
 
-import type { SourceFile, Node, ts } from "ts-morph";
-import type { ExtensionContext, TextDocument, Disposable } from "vscode";
+const decorationType = window.createTextEditorDecorationType({
+  backgroundColor: "red",
+  border: "1px solid white",
+});
 
-/**
- * ? ====== FUNCTION DECLARATION ======
- */
+const decorateFile = (editor: TextEditor) => {
+  const content = editor.document.getText();
+  const lines = content.split("\n");
 
-/**
- * Multiply a character.
- *
- * char - The character to multiply
- * amount - The amount of times the character is multiplied
- *
- * Ex: multChar("a", 5) -> "aaaaa"
- *     multChar(" ", 2) -> "  "
- */
-const multChar = (char: string, amount: number): string => {
-  return Array(amount).fill(char).join("");
-};
+  const regex = /const/;
 
-/**
- * Update the [lines] array to contain the nodes with their hierarchy.
- *
- * lines - The array containing the lines
- * node - The actual node
- * indent - The indentation (the space before the node's name)
- */
-const parser = (lines: string[], node: Node, indent = 0): void => {
-  lines.push(multChar(" ", indent) + node.getKindName());
-  node.forEachChild((child) => parser(lines, child, indent + 2));
-};
+  let ranges: Range[] = [];
 
-const countNodes = (node: Node<ts.Node>): number => {
-  let total = 1;
+  for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+    const match = lines[lineIndex].match(regex);
+    console.log(`On line ${lineIndex}, the match is :`, match);
 
-  for (const child of node.getChildren()) {
-    total += countNodes(child);
-  }
+    if (match !== null && match.index !== undefined) {
+      const range = new Range(
+        new Position(lineIndex, match.index),
+        new Position(lineIndex, match.index + match[1].length)
+      );
 
-  return total;
-};
-
-/**
- * ? ====== CLASS DECLARATION PART ======
- */
-class TreeProvider implements TreeDataProvider<Node<ts.Node>> {
-  constructor(private root: SourceFile) {}
-
-  getTreeItem(element: Node<ts.Node>): TreeItem | Thenable<TreeItem> {
-    return new TreeItem(
-      element.getKindName(),
-      TreeItemCollapsibleState.Expanded
-    );
-  }
-
-  getChildren(
-    element?: Node<ts.Node> | undefined
-  ): ProviderResult<Node<ts.Node>[]> {
-    if (element === undefined) {
-      return [this.root];
+      ranges.push(range);
     }
-
-    return element.getChildren();
   }
-}
+  console.log(ranges);
+  editor.setDecorations(decorationType, ranges);
+};
 
-/**
- * ? ====== EXTENSION PART ======
- *
- * Activation Events : https://code.visualstudio.com/api/references/activation-events
- *
- * The [activate] and [deactivate] functions are need by the vscode extension library.
- * The [activate] function runs at start of extensions and [deactivate] at the end.
- */
 export function activate(context: ExtensionContext) {
-  // A message printed on the debug console when the extension loads
-  console.log('Congratulations, your extension "firstlesson" is now active!');
+  console.log("Activated the extension");
+  const saveEvent = workspace.onWillSaveTextDocument((event) => {
+    console.log("Saved the file");
+    const editor = window.visibleTextEditors.filter(
+      (editor) => editor.document.uri === event.document.uri
+    )[0];
+    console.log("Editor :", editor);
 
-  // Project instance created by ts-morph, this one is needed by the ts-morph library
-  //
-  // the "useInMemorySystem" property is used to virtualize the saved files by
-  // ts-morph
-  const project = new Project({
-    useInMemoryFileSystem: true,
+    decorateFile(editor);
   });
 
-  let sourceFile: SourceFile | undefined;
-  let treeView: Disposable | undefined;
-
-  // Event Handler
-  //
-  // Triggered when the user saves the document (ctrl + s or Save File)
-  let saveEvent = workspace.onDidSaveTextDocument((file: TextDocument) => {
-    // If there is no view loaded or the language of the current file
-    // is not "typescript", we exit the function
-    if (file.languageId !== "typescript") {
-      return;
-    }
-
-    // ts-morph creates a temporary file with the content of the file
-    // we are currently working on, it creates its own file so as to
-    // handle its content
-    sourceFile = project.createSourceFile("__temp__.ts", file.getText());
-
-    if (!treeView) {
-      treeView = window.registerTreeDataProvider(
-        "treeView",
-        new TreeProvider(sourceFile)
-      );
-      context.subscriptions.push(treeView);
-    }
-  });
-
-  // Command Registration
-  //
-  // Register a command named "firstlesson.SeeTree", check the "package.json" file to
-  // see the registered command.
-  let disposable = commands.registerCommand("firstlesson.SeeTree", () => {
-    console.log("Command called !");
-  });
-
-  let countNodesCommand = commands.registerCommand(
-    "firstlesson.CountNodes",
-    () => {
-      if (sourceFile === undefined) {
-        return;
-      }
-
-      const count = countNodes(sourceFile);
-      window.showInformationMessage(
-        `There are ${count} nodes in the abstract syntax tree.`
-      );
-    }
-  );
-
-  // Push the event handler and the command registration to the extension subscriptions
-  // this is not compulsary but a good practice, it helps the extension handle things better.
-  context.subscriptions.push(disposable, saveEvent, countNodesCommand);
+  context.subscriptions.push(saveEvent);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
