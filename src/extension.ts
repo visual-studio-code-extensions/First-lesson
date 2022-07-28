c29f8477192ab12a19989388665ba3e4c65b1259
Lola2018 <eftuwakjira@yahoo.com>
2022-07-28 07:50:15 -0700

    first commit

--- src/extension.ts
+++ src/extension.ts
@@ -1,50 +1,34 @@
-import { window, workspace, Range, Position } from "vscode";
+import { languages } from "vscode";
 
-import type { ExtensionContext, TextEditor, DecorationOptions } from "vscode";
-
-const decorationType = window.createTextEditorDecorationType({
-  backgroundColor: "red",
-  border: "1px solid white",
-});
-
-const decorateFile = (editor: TextEditor) => {
-  const content = editor.document.getText();
-  const lines = content.split("\n");
-
-  const regex = /const/;
-
-  let ranges: Range[] = [];
-
-  for (let lineIndex = 0; lineIndex < lines.length; lineIndex++) {
-    const match = lines[lineIndex].match(regex);
-    console.log(`On line ${lineIndex}, the match is :`, match);
-
-    if (match !== null && match.index !== undefined) {
-      const range = new Range(
-        new Position(lineIndex, match.index),
-        new Position(lineIndex, match.index + match[1].length)
-      );
-
-      ranges.push(range);
-    }
-  }
-  console.log(ranges);
-  editor.setDecorations(decorationType, ranges);
-};
+import type { ExtensionContext } from "vscode";
 
 export function activate(context: ExtensionContext) {
   console.log("Activated the extension");
-  const saveEvent = workspace.onWillSaveTextDocument((event) => {
-    console.log("Saved the file");
-    const editor = window.visibleTextEditors.filter(
-      (editor) => editor.document.uri === event.document.uri
-    )[0];
-    console.log("Editor :", editor);
 
-    decorateFile(editor);
+  const provider = languages.registerHoverProvider("typescript", {
+    provideHover(document, position, token) {
+      const range = document.getWordRangeAtPosition(position);
+      const word = document.getText(range);
+
+      if (word === "const") {
+        return {
+          contents: ["This is a const keyword."],
+        };
+      }
+
+      if (word === "console") {
+        return {
+          contents: ["This is a console keyword."],
+        };
+      }
+
+      return {
+        contents: ["Hello World"],
+      };
+    },
   });
 
-  context.subscriptions.push(saveEvent);
+  context.subscriptions.push(provider);
 }
 
 export function deactivate() {}
